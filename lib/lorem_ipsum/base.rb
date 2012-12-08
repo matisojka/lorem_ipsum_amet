3696b903b983d84db4f22b56639452aee85a8aed
Mateusz Sojka <yagooar@gmail.com>
2012-12-08 12:00:23 +0100

    Allow to specify the amount of characters

--- lib/lorem_ipsum/base.rb
+++ lib/lorem_ipsum/base.rb
@@ -3,11 +3,19 @@ module LoremIpsumAmet
 
     class << self
 
-      def lorem_ipsum
-        paragraphs.first
+      def lorem_ipsum(characters = nil)
+        if characters.nil?
+          paragraphs.first
+        else
+          lorem_ipsum_characters(characters)
+        end
       end
 
       alias text lorem_ipsum
+
+      def full
+        raw_text
+      end
 
       private
 
@@ -17,6 +25,14 @@ module LoremIpsumAmet
 
       def paragraphs
         raw_text.split("\n")
+      end
+
+      def lorem_ipsum_characters(characters)
+        times_to_repeat = (characters / raw_text.length) + 1
+
+        text = ([raw_text] * times_to_repeat).join("\n")
+
+        text[0..characters]
       end
 
     end
--- spec/lorem_ipsum/base_spec.rb
+++ spec/lorem_ipsum/base_spec.rb
@@ -8,15 +8,34 @@ describe LoremIpsumAmet::Base do
     %Q(Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris. Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus. Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus. Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci. Fusce id felis. Vivamus sollicitudin metus eget eros.)
   end
 
+  let(:raw_text) { LoremIpsumAmet::Text.raw }
+
   describe '.lorem_ipsum' do
     it 'returns the first paragraph by default' do
       expect(subject.lorem_ipsum).to eq(first_paragraph)
     end
+
+    it 'returns a given amount of characters' do
+      expect(subject.lorem_ipsum(15)).to eq(first_paragraph[0..15])
+    end
+
+    it 'returns a given amount of characters (more characters than raw text length)' do
+      long_text = [raw_text, raw_text].join("\n")[0..6000]
+
+      expect(subject.lorem_ipsum(6000)).to eq(long_text)
+    end
+
   end
 
   describe '.text' do
     it 'is an alias of .lorem_ipsum' do
       expect(subject.method(:text)).to eq(subject.method(:lorem_ipsum))
+    end
+  end
+
+  describe '.full' do
+    it 'returns the full text' do
+      expect(subject.full).to eq(raw_text)
     end
   end
 
