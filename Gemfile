4cf3c224aa4009603292f0de917c711995d11401
Mateusz Sojka <yagooar@gmail.com>
2012-12-08 11:11:18 +0100

    Basic structure

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+group :development do
+  gem 'rspec', '>= 2.12.0'
+  gem 'rdoc', '~> 3.12'
+  gem 'bundler', '>= 1.1.3'
+  gem 'jeweler', '~> 1.8.4'
+  gem 'simplecov'
+  gem 'pry'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+# Run only when needed
+# require 'simplecov'
+# SimpleCov.start
+
+require 'rspec'
+require 'lorem_ipsum'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+end
